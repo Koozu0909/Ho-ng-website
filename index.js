20bc1baf532900baaee6d197692a62a484aeafd3
Koozu0909 <87105119+Koozu0909@users.noreply.github.com>
2021-11-01 22:57:22 +0700

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+/********  SHOWBARS  *********/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle =  document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+const navLink = document.querySelectorAll('.nav-link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+
+
+// Service swiper
+
+let swiper = new Swiper(".service__container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 20,
+    coverflowEffect: {
+        rotate: 0,
+    },
+})
